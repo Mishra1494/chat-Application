23a195732a594068fc35373647345ab52d5ba134
Mishra1494 <mishraraunak117@gmail.com>
2026-01-09 15:51:21 +0530

    done auth

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,11 @@ import userModel from "../models/userModels.js";
 import checkUserExists from "../utils/checkUser.js";
 import hashPassword from "../utils/hashPassWord.js";
 import jwtWebToken from "../utils/jwtWebToken.js";
+import bcrypt from 'bcryptjs';
+
+
+
+
 
 export const register =async (req,res)=>{
     try{
@@ -35,4 +40,42 @@ export const register =async (req,res)=>{
     }catch(error){
         res.status(500).json({success:false, message : error.message});
     }
+}
+
+
+
+
+
+
+export const login = async (req,res)=>{
+    try{
+        const {email , userName, password} = req.body;
+        if(!email && !userName){
+            return res.status(400).json({success : false,message : "email or userName is required"});
+        }
+        const user = await userModel.findOne({$or :[email ? {email} : null, userName ? {userName} : null].filter(Boolean)});
+        if(!user){
+            res.status(404).json({success : false, message : "user not found"});
+        }
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if(!isPasswordMatch){
+            return res.status(400).json({success : false, message : "invalid credentials"});
+        }
+
+        const generateToken = jwtWebToken(user._id,res);
+        res.status(200).json({success : true,token: generateToken, message : "login successful",user});
+    }catch(error){
+        res.status(500).json({success:false, message : error.message});
+    }
+}
+
+
+
+export const logout = (req,res)=>{
+    res.clearCookie("token",{
+        httpOnly : true, 
+        sameSite : "strict",
+        secure : process.env.NODE_ENV !== "production"
+    });
+    res.status(200).json({success : true, message : "logout successful"});
 }
--- backend/utils/jwtWebToken.js
+++ backend/utils/jwtWebToken.js
@@ -1,5 +1,9 @@
 import jwt from 'jsonwebtoken';
+// import dotenv from "dotenv";
+// dotenv.config({path : "./.env"});
+
 const generateToken  =  (userId,res) =>{
+    console.log(process.env.JWT_SECRET);
     const token = jwt.sign({id : userId},process.env.JWT_SECRET,{expiresIn : '7d'});
     res.cookie("token",token,{
         maxAge : 7*24*60*60*1000,
